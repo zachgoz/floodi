c65e9da66eb4ff940970aa1581ccb538bc4a2800
zachgoz <zacharygosling@gmail.com>
2025-07-24 16:36:24 +0000

    Create ionic react starter app

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const segments = [
-  {
-    id: 'canal',
-    name: 'Canal Dr',
-    elevationFtMLLW: 1.5,
-    stationId: '8658163',
-    sensorPlatform: 'SUNNYD_CB_02'
-  }
-];
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function formatYMD(date: Date): string {
-  return date.toISOString().slice(0,10).replace(/-/g, '');
-}
